0c3b05eee6fc4539168ec17c04ae5ea480ab6269
Kristján Oddsson <koddsson@gmail.com>
2019-02-25 18:15:33 +0000

    move user agent testing into function

--- emoji-detection.js
+++ emoji-detection.js
@@ -1,11 +1,11 @@
 /* @flow */
 
-const onWindows7 = /\bWindows NT 6.1\b/.test(navigator.userAgent)
-const onWindows8 = /\bWindows NT 6.2\b/.test(navigator.userAgent)
-const onWindows81 = /\bWindows NT 6.3\b/.test(navigator.userAgent)
-const onLinux = /\bLinux\b/.test(navigator.userAgent)
-const onFreeBSD = /\bFreeBSD\b/.test(navigator.userAgent)
-
 export function isEmojiSupported(): boolean {
+  const onWindows7 = /\bWindows NT 6.1\b/.test(navigator.userAgent)
+  const onWindows8 = /\bWindows NT 6.2\b/.test(navigator.userAgent)
+  const onWindows81 = /\bWindows NT 6.3\b/.test(navigator.userAgent)
+  const onFreeBSD = /\bFreeBSD\b/.test(navigator.userAgent)
+  const onLinux = /\bLinux\b/.test(navigator.userAgent)
+
   return !(onWindows7 || onWindows8 || onWindows81 || onLinux || onFreeBSD)
 }
