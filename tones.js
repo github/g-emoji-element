820ef3fddcc86fc0fc6f4661eaf9df8593998c79
Mu-An Chiou <me@muanchiou.com>
2019-10-11 18:32:19 -0400

    Check modifiable on full emoji sequence
    - Checking first codePoint is not indicative of full sequence skin tone
      support.
    
    - Some Emoji has default male state. For example,
      [runner, zwj, male, variation_16] and [runner] both represent the male
      runner emoji. So when checking modifiability, we need to sanitize the
      modifiers out.
    
      This is better than checking for the first code point because family
      emoji for example, is made out of [man, woman, boy, girl], each is
      individually modifiable, but not as a whole.

--- tones.js
+++ tones.js
@@ -6,17 +6,21 @@ const ZERO_WIDTH_JOINER = '\u{200d}'
 const VARIATION_16 = 0xfe0f
 
 export function applyTone(sequence: string, tone: number): string {
+  const sequenceWithToneRemoved = removeTone(sequence)
+  if (!isModifiable(sequenceWithToneRemoved)) return sequence
   const modifier = toneModifier(tone)
   if (!modifier) return sequence
-  return sequence
+  return sequenceWithToneRemoved
     .split(ZERO_WIDTH_JOINER)
     .map(emoji => (isModifiable(emoji) ? tint(emoji, modifier) : emoji))
     .join(ZERO_WIDTH_JOINER)
 }
 
 export function applyTones(sequence: string, tones: Array<number>): string {
+  const sequenceWithToneRemoved = removeTone(sequence)
+  if (!isModifiable(sequenceWithToneRemoved)) return sequence
   const modifiers = tones.map(t => toneModifier(t))
-  return sequence
+  return sequenceWithToneRemoved
     .split(ZERO_WIDTH_JOINER)
     .map(emoji => {
       if (!isModifiable(emoji)) return emoji
