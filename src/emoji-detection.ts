f7d07487a4155fe5d3d4571f7797c573b8414d79
Mu-An 慕安 <me@muanchiou.com>
2020-07-15 16:00:49 -0400

    Detect non-Androids

--- src/emoji-detection.ts
+++ src/emoji-detection.ts
@@ -3,7 +3,7 @@ export function isEmojiSupported(): boolean {
   const onWindows8 = /\bWindows NT 6.2\b/.test(navigator.userAgent)
   const onWindows81 = /\bWindows NT 6.3\b/.test(navigator.userAgent)
   const onFreeBSD = /\bFreeBSD\b/.test(navigator.userAgent)
-  const onLinux = /\bLinux\b/.test(navigator.userAgent)
+  const onLinux = /\bLinux\b/.test(navigator.userAgent) && !/\bAndroid\b/.test(navigator.userAgent)
 
   return !(onWindows7 || onWindows8 || onWindows81 || onLinux || onFreeBSD)
 }
